d86709ef0501afbd889a9f5e23359110cfa18b46
Winson Tan <winsont89@gmail.com>
2019-03-05 15:36:28 +1100

    new seeds

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,18 @@
+class PhotosController < ApplicationController
+  # def new
+  #   @photo = Photo.new
+  #   authorize @tour
+  # end
+
+  # def create
+  #   @tour = Tour.new(tour_params)
+  #   authorize @tour
+  #   @tour.user = current_user
+  #   @tour.save
+  #   if @tour.save
+  #     redirect_to tour_path(@tour)
+  #   else
+  #     render :new
+  #   end
+  # end
+end
