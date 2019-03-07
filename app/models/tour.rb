f04c9f9d889aa1fc371fd43a1004b27f482fd366
Alex Vassiliou <alex@20diamond.com>
2019-03-07 13:34:08 +1100

    searchbar

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,10 +2,9 @@ class Tour < ApplicationRecord
   belongs_to :user
   has_many :tracks, dependent: :destroy
   has_many :photos, dependent: :destroy
-  has_many :reviews, dependent: :destroy
 
   geocoded_by :start_location
-  after_validation :geocode, if: :key => "value", will_save_change_to_start_location?
+  after_validation :geocode, if: :will_save_change_to_start_location?
 
   validates :title, presence: true, uniqueness: true
   validates :start_location, presence: true
