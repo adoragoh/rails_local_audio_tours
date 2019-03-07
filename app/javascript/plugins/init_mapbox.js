d3a23ef1dcb6243577c6cb3db4952799d093f179
Alex Vassiliou <alex@20diamond.com>
2019-03-07 16:30:50 +1100

    map route

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,29 +12,67 @@ const initMapbox = () => {
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const markers = JSON.parse(mapElement.dataset.markers);
+
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [ markers[0].lng, markers[0].lat],
+      zoom: 12
     });
-    const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // <-- add this
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup) // <-- add this
-      .addTo(map);
+    // new mapboxgl.Marker()
+    //   .setLngLat([ marker.lng, marker.lat ])
+    //   .setPopup(popup) // <-- add this
+    //   .addTo(map);
     });
 
-    map.addControl(new mapboxgl.GeolocateControl({
+    var userLocationControl = new mapboxgl.GeolocateControl({
       positionOptions: {
         enableHighAccuracy: true
       },
-      trackUserLocation: true
-    }));
+      showUserLocation: true
+    })
 
-    fitMapToMarkers(map, markers);
+    map.addControl(userLocationControl);
+
+      var directionsControl = new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+        interactive: false,
+        profile:'mapbox/walking',
+        controls:{
+          inputs:false,
+          instructions:false,
+          profileSwitcher:false
+        }
+      })
+
+      map.addControl(directionsControl, 'top-left');
+
+      const addRouteToMap = (currentUserLat, currentUserLon) => {
+        directionsControl.setOrigin([currentUserLon, currentUserLat]);
+        directionsControl.setDestination([mapElement.dataset.destinationLon, mapElement.dataset.destinationLat]);
+      }
+
+
+      map.on('load', function(){
+        userLocationControl.trigger();
+      });
+
+      userLocationControl.on('geolocate', function(userLocation) {
+        addRouteToMap(userLocation.coords.latitude, userLocation.coords.longitude);
+      });
+
+      userLocationControl.on('error', function(error) {
+        alert(error.message);
+      });
+
+
+    // fitMapToMarkers(map, markers);
     // addMarkersToMap(map, markers);
   }
+
 
 
 };
