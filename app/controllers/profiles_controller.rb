00ad36f1e4e220ce844948e8e33c3c081c408a70
Adora Goh <heyadora@gmail.com>
2019-03-11 18:14:27 +1100

    Profile link in sidebar to go to edit user page, fixed tour show page breaking if user not signed in

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,13 +2,21 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tours = policy_scope(Tour)
-    @favourites = policy_scope(Favourite)
+    @user = current_user
+    @email = current_user.email
+    @first_name = current_user.first_name
+    @last_name = current_user.last_name
 
-    @my_tours = @tours.where(user_id: current_user).order(created_at: :desc)
-    @my_favourites = @favourites.where(user_id: current_user).order(created_at: :desc)
+    # 1. Form already filled in with user attributes
+    # 2. When you submit, it updates the user's information
 
-    @just_favourited = params[:just_favourited] == "true"
-    @just_created_tour = params[:just_created_tour] == "true"
+    # @tours = policy_scope(Tour)
+    # @favourites = policy_scope(Favourite)
+
+    # @my_tours = @tours.where(user_id: current_user).order(created_at: :desc)
+    # @my_favourites = @favourites.where(user_id: current_user).order(created_at: :desc)
+
+    # @just_favourited = params[:just_favourited] == "true"
+    # @just_created_tour = params[:just_created_tour] == "true"
   end
 end
