c050226e804bb48f2895271ba6d92a7964bbce15
Alex Vassiliou <alex@20diamond.com>
2019-03-13 17:02:09 +1100

    review linking added into app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import { initGotoMap } from '../components/goto_map';
 import { initZomato } from '../components/zomato.js';
 import { autoPlay} from '../components/autoplay.js';
 import '../components/js-auto-submit';
+import '../components/js-close-modal';
 
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { hello } from '../components/navbar_v2.js';
