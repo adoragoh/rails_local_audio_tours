8668e44d005cdfdccad3c67d2a9967500b36e048
Adora Goh <heyadora@gmail.com>
2019-04-08 18:52:40 +1000

    add typeform page to /register

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [ :home, :register ]
   def home
   end
+
+  def register
+  end
+
 end
